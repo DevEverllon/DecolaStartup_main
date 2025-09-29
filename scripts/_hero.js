8278c4388e60ff53fed80834d55893fdca569b80
Dev Éverllon <everllonsouza5@gmail.com>
2025-09-29 19:00:07 -0300

    update

---
+++ scripts/_hero.js
@@ -0,0 +1,16 @@
+// gsap.from(".hero-content", {
+//     x: -100,
+//     opacity: 0,
+//     duration: 1.5,
+//     stagger: 0.2, 
+//     ease: "power2.out"
+// });
+
+// gsap.from(".hero-image div", {
+//     x:100,
+//     opacity: 0,
+//     duration: 1.5,
+//     stagger: 0.2, 
+//     ease: "power2.out"
+
+// });
